e78fe9ce3794f7c8ba5b9739d445c0325bae4623
Tejas Rawal <trawal90@gmail.com>
2014-02-05 01:04:20 -0600

    Simple chat server using node.js

--- public/chat.js
+++ public/chat.js
@@ -1,14 +1,27 @@
 window.onload = function() {
 
-    var messages = [];
-    var socket = io.connect('http://localhost:3005');
-    var field = document.getElementById("field");
-    var sendButton = document.getElementById("send");
-    var content = document.getElementById("content");
+  var messages = [];
+  var socket = io.connect('http://localhost:3005');
+  var field = document.getElementById("field");
+  var sendButton = document.getElementById("send");
+  var content = document.getElementById("content");
 
-    sendButton.onclick = function() {
-        var text = field.value;
-        socket.emit('send', { message: text });
-    };
+  sendButton.onclick = function() {
+      var text = field.value;
+      socket.emit('send', { message: text });
+  };
 
+  socket.on('message', function (data) {
+    if(data.message) {
+      messages.push(data.message);
+      var html = '';
+      for(var i=0; i<messages.length; i++) {
+          html += messages[i] + '<br />';
+      }
+      content.innerHTML = html;
+    }
+    else {
+      console.log("There is a problem:", data);
+    }
+  });
 }
